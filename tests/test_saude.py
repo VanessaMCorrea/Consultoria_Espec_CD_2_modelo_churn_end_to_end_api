b1edf4c2954a3e68f4547bb13b50add9ba5b2946
Vanessa Marques Correa <v.marquescorrea@gmail.com>
2026-04-07 01:26:27 +0000

    Removendo o teste que da erro em test_saude

--- tests/test_saude.py
+++ tests/test_saude.py
@@ -2,7 +2,3 @@
 def test_pytest_funcionando():
     """Confirma que o pytest encontrou e executou este arquivo."""
     assert 1 + 1 == 2
-
-def test_que_vai_falhar():
-    """Este teste falha de propósito."""
-    assert 1 + 1 == 3
